7fa03dfa533cc318dd0fc687ea2bed5e6f432c21
Abdurahmon007 <elbekmovlonov@gmail.com>
2024-01-06 14:01:09 +0500

    fetching posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
-import { useEffect } from "react";
 import "./App.css";
-import { useAppDispatch, useAppSelector } from "./hooks/redux";
-import { fetchUsers } from "./store/reducers/actionCreators";
+// import { useEffect } from "react";
+// import { useAppDispatch, useAppSelector } from "./hooks/redux";
+// import { fetchUsers } from "./store/reducers/actionCreators";
+import PostContainer from "./components/PostContainer";
 
 function App() {
-  const dispatch = useAppDispatch();
-  const { users, isLoading, error } = useAppSelector(
-    (state) => state.userReducer
-  );
-  useEffect(() => {
-    dispatch(fetchUsers());
-    // eslint-disable-next-line
-  }, []);
+  // const dispatch = useAppDispatch();
+  // const { users, isLoading, error } = useAppSelector(
+  //   (state) => state.userReducer
+  // );
+  // useEffect(() => {
+  //   dispatch(fetchUsers());
+  //   // eslint-disable-next-line
+  // }, []);
   return (
     <div className="App">
-      {isLoading && <h1>Loading...</h1>}
+      {/* {isLoading && <h1>Loading...</h1>}
       {error && <h1>{error}</h1>}
-      {JSON.stringify(users, null, 2)}
+      {JSON.stringify(users, null, 2)} */}
+      <PostContainer />
     </div>
   );
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,17 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./reducers/UserSlice";
+import { postApi } from "../service/postService";
 
-const rootReducer = combineReducers({ userReducer });
+const rootReducer = combineReducers({
+  userReducer,
+  [postApi.reducerPath]: postApi.reducer,
+});
 
 export const setupStore = () => {
   return configureStore({
     reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(postApi.middleware),
   });
 };
 
